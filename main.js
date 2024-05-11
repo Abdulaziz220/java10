0d26e49a335c8829f9a56199548eab9d97b0f414
Your Name <you@example.com>
2024-05-11 13:04:32 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let a = +prompt("birinchi soni kiriting");
+let b = +prompt("ikkinchi soni kiriting")
+let c;
+
+
+function  son () {
+    if (c === Math.max(a, b)) {
+        alert("Eng katta son" + a)
+    } else if (a === b) {
+        alert(a + "va" + b + "teng")
+    }else{
+        alert("Eng katta son" + b)
+    }
+}
+
+son(a,b);
+
+
